bf3a78e304caefd057a5efe245dc28e1738d7ad6
MathsPrado <matheusprado55@gmail.com>
2020-02-09 20:46:54 -0300

    add project

---
+++ SalesMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesMVC.Models;
+
+namespace SalesMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Department> list = new List<Department>();
+
+            list.Add(new Department { Id = 1, Name = "Eletronicos" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+       
+            return View(list);
+        }
+    }
+}
---
+++ SalesMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SalesMVC.Models.ViewModels;
+
+namespace SalesMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Salles Web MVC App from c# Course";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

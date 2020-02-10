1196323fb6aba759bf907db91c9727f140a349a8
MathsPrado <matheusprado55@gmail.com>
2020-02-09 23:28:59 -0300

    MySQL adaptation and first migration

--- SalesMVC/Startup.cs
+++ SalesMVC/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
 using SalesMVC.Models;
 
 namespace SalesMVC
@@ -37,7 +32,8 @@ namespace SalesMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesMVCContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesMVCContext"), Builder =>
+                         Builder.MigrationsAssembly("SalesMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
